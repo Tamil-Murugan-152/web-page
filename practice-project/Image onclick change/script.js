aa8263a5aa9be4ad5b1ffd79bf11a1e54d89b117
Tamil-Murugan-152 <tamilchelvanmurugan@gmail.com>
2025-09-18 21:50:14 +0530

    Projects

---
+++ practice-project/Image onclick change/script.js
@@ -0,0 +1,8 @@
+const key1=document.getElementById("im1");
+const key2=document.getElementById("im2");
+key1.addEventListener("click",()=>{
+key1.classList.toggle("one");
+})
+key2.addEventListener("click",()=>{
+key2.classList.toggle("two");
+})
